2c90169cb3ae57effa34920075cbf12d54500a3b
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2024-01-20 18:08:59 +0100

    fetch products from api & add extra products

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,33 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './pages/Home';
+import { useEffect, useState } from 'react';
+import extraProducts from './extraProducts';
 
 const Router = () => {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((res) => {
+        if (res.status >= 400) {
+          throw new Error('Server Error');
+        }
+        return res.json();
+      })
+      .then((data) => setProducts([...data, ...extraProducts]))
+      .catch((err) => setError(err))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (error) return <p>A network error was encountered</p>;
+  if (loading) return <p>Loading...</p>;
+
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <Home />,
+      element: <Home products={products} />,
     },
   ]);
 
---
+++ src/extraProducts.js
@@ -0,0 +1,105 @@
+import {
+  bigShoe1,
+  bigShoe2,
+  bigShoe3,
+  shoe4,
+  shoe5,
+  shoe6,
+  shoe7,
+} from './assets/images';
+
+const extraProducts = [
+  {
+    id: 21,
+    title: 'New Balance 740',
+    price: 40.2,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, dolore minima! Corporis eum voluptatum amet sapiente! Adipisci, aut corporis nisi ab, a nam sed vel',
+    category: 'shoes',
+    image: bigShoe1,
+    rating: {
+      rate: 4.5,
+      count: 220,
+    },
+  },
+  {
+    id: 22,
+    title: 'Carhart air 430',
+    price: 53.9,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, dolore minima! Corporis eum voluptatum amet sapiente! Adipisci, aut corporis nisi ab, a nam sed vel',
+    category: 'shoes',
+    image: bigShoe2,
+    rating: {
+      rate: 4.2,
+      count: 500,
+    },
+  },
+  {
+    id: 23,
+    title: 'Adidas QuantumFlex BoostStride',
+    price: 39.9,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, dolore minima! Corporis eum voluptatum amet sapiente! Adipisci, aut corporis nisi ab, a nam sed vel',
+    category: 'shoes',
+    image: bigShoe3,
+    rating: {
+      rate: 3.6,
+      count: 160,
+    },
+  },
+  {
+    id: 24,
+    title: 'Nike AeroPulse ZoomShift',
+    price: 69.9,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, dolore minima! Corporis eum voluptatum amet sapiente! Adipisci, aut corporis nisi ab, a nam sed vel',
+    category: 'shoes',
+    image: shoe4,
+    rating: {
+      rate: 4.8,
+      count: 531,
+    },
+  },
+  {
+    id: 25,
+    title: 'K-Swiss SolarGlide FlowTech',
+    price: 89.9,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, dolore minima! Corporis eum voluptatum amet sapiente! Adipisci, aut corporis nisi ab, a nam sed vel',
+    category: 'shoes',
+    image: shoe5,
+    rating: {
+      rate: 4.1,
+      count: 432,
+    },
+  },
+  {
+    id: 26,
+    title: 'Nike ShadowPulse StealthBlack',
+    price: 67.4,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, dolore minima! Corporis eum voluptatum amet sapiente! Adipisci, aut corporis nisi ab, a nam sed vel',
+    category: 'shoes',
+    image: shoe6,
+    rating: {
+      rate: 4.4,
+      count: 332,
+    },
+  },
+  {
+    id: 27,
+    title: 'New Balance 388',
+    price: 87.6,
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, dolore minima! Corporis eum voluptatum amet sapiente! Adipisci, aut corporis nisi ab, a nam sed vel',
+    category: 'shoes',
+    image: shoe7,
+    rating: {
+      rate: 4.9,
+      count: 765,
+    },
+  },
+];
+
+export default extraProducts;
