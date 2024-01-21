c674e1d609fd3263ee69bca22ec6a0c1964c04bb
Berget1411 <ludvig.benjaminbergstrom@gmail.com>
2024-01-21 18:15:01 +0100

    add styling properties to button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,19 @@
-const Button = ({ label, iconURL }) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  textColor,
+  borderColor,
+}) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-emerald-600 rounded-full text-white border-emerald-600 hover:brightness-110 transition'>
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : 'bg-emerald-600 text-white border-emerald-600 hover:brightness-110 transition'
+      }`}
+    >
       {label}
       {iconURL && (
         <img
